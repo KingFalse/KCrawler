6a6ca5a231d89f6961eef2b3fc594afbf677fd78
ZFang <FangZhuo@WeCash.Net>
2018-11-08 18:19:23 +0800

    init

---
+++ src/main/java/me/kagura/kcrawler/KCrawlerApplication.java
@@ -0,0 +1,12 @@
+package me.kagura.kcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KCrawlerApplication.class, args);
+    }
+}
