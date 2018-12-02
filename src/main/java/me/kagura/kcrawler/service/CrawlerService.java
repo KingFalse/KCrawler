de98b191e3052f96a5acabde0a4f760e0cffe1dc
鹞之神乐 <yzsl@live.com>
2018-12-02 21:24:28 +0800

    update

--- src/main/java/me/kagura/kcrawler/service/CrawlerService.java
+++ src/main/java/me/kagura/kcrawler/service/CrawlerService.java
@@ -5,12 +5,13 @@ import me.kagura.kcrawler.entity.CrawlerTask;
 import org.jsoup.Connection;
 import org.jsoup.helper.StringUtil;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CrawlerService {
@@ -20,7 +21,7 @@ public class CrawlerService {
 
     public void doCrawler(CrawlerTask task) {
         //用于存储需要爬取的url
-        Set<String> listTargetUrls = Collections.synchronizedSet(new TreeSet<>());
+        List<String> listTargetUrls = Collections.synchronizedList(new ArrayList<>());
         try {
             Connection.Response response = jJsoup.connect(task.getStartUrl()).method(Connection.Method.GET).execute();
             Document document = response.parse();
@@ -30,40 +31,34 @@ public class CrawlerService {
                 listTargetUrls.add(task.getStartUrl());
             } else {
                 //有链接选择器
-                Elements targetHref = document.select(task.getTargetSelector());
-                targetHref.forEach(target -> listTargetUrls.add(target.attr("abs:href")));
+                document.select(task.getTargetSelector()).forEach(target -> listTargetUrls.add(target.attr("abs:href")));
             }
 
-            Set<String> listPageUrls = new TreeSet<>();
+            List<String> listPageUrls = Collections.synchronizedList(new ArrayList<>());
+            listPageUrls.add(task.getStartUrl().replaceAll("#$", ""));
 
             boolean onePage = StringUtil.isBlank(task.getPageSelector());
-            if (!onePage) {
-                document.select(task.getPageSelector()).forEach(e -> listPageUrls.add(e.attr("abs:href")));
-                listPageUrls.remove(task.getStartUrl());
-            }
 
+            //计算循环总次数，i<0无限循环，i=0循环一次，i>0则循环i次
+            int loopCount = (onePage ? 1 : (task.getTargetPageCount() < 0 ? Integer.MAX_VALUE : task.getTargetPageCount()) - 1);
+            for (int i = 0; i < loopCount; i++) {
+                document.select(task.getPageSelector()).forEach(e -> {
+                    String absurl = e.attr("abs:href").replaceAll("#$", "");
+                    if (!listPageUrls.contains(absurl)) {
+                        listPageUrls.add(absurl);
+                    }
+                });
+                document.select(task.getTargetSelector()).forEach(e -> listTargetUrls.add(e.attr("abs:href")));
 
-            for (int i = 0; i < (onePage ? 1 : task.getTargetPageCount() - 1); i++) {
-                if (i >= listPageUrls.size()) {
+                if (i >= listPageUrls.size() - 1) {
                     break;
                 }
-                if (onePage) {
-                    document.select(task.getTargetSelector()).forEach(e -> listTargetUrls.add(e.attr("abs:href")));
-                } else {
-                    if (i == 0) {
-                        document.select(task.getTargetSelector()).forEach(e -> listTargetUrls.add(e.attr("abs:href")));
-                    }
-                    document = jJsoup.connect((String) listPageUrls.toArray()[i]).method(Connection.Method.GET).execute().parse();
-                    document.select(task.getTargetSelector()).forEach(e -> listTargetUrls.add(e.attr("abs:href")));
-                    document.select(task.getPageSelector()).forEach(e -> listPageUrls.add(e.attr("abs:href")));
-                }
+                document = jJsoup.connect(listPageUrls.get(i + 1)).method(Connection.Method.GET).execute().parse();
             }
-
-
-            listPageUrls.forEach(url-> System.err.println(url));
+            listPageUrls.forEach(url -> System.err.println(url));
             System.err.println("==========================");
+            System.err.println(listPageUrls.size());
             System.err.println(listTargetUrls.size());
-            listTargetUrls.forEach(url-> System.err.println(url));
 
 
         } catch (IOException e) {
--- src/test/java/me/kagura/kcrawler/service/CrawlerServiceTest.java
+++ src/test/java/me/kagura/kcrawler/service/CrawlerServiceTest.java
@@ -20,10 +20,17 @@ public class CrawlerServiceTest {
     public void doCrawler() {
 
         CrawlerTask crawlerTask = new CrawlerTask();
-        crawlerTask.setStartUrl("https://www.kagura.me/");
-        crawlerTask.setPageSelector("li.next-page a");
-        crawlerTask.setTargetSelector("article header h2 a");
-        crawlerTask.setTargetPageCount(3);
+//        crawlerTask.setStartUrl("https://www.kagura.me/");
+//        crawlerTask.setPageSelector("li.next-page a");
+//        crawlerTask.setTargetSelector("article header h2 a");
+//        crawlerTask.setTargetPageCount(3);
+
+        crawlerTask.setStartUrl("https://www.jb51.net/softs/list38_1.html");
+        crawlerTask.setPageSelector(".plist a");
+        crawlerTask.setTargetSelector(".top-tit .tit a");
+//        crawlerTask.setTargetPageCount(-1);//无限翻页
+//        crawlerTask.setTargetPageCount(0);//不翻页
+        crawlerTask.setTargetPageCount(3);//指定页数
 
         crawlerService.doCrawler(crawlerTask);
     }
